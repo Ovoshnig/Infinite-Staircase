529b90c3f660e48fbba873f140f3817babdead7c
Ovoshnig <boost45644@gmail.com>
2024-06-16 21:21:23 +0700

    Added character jump animation
    Added player character jump animation, it needs some work

--- Assets/Scripts/Player/PlayerAnimator.cs
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -6,6 +6,7 @@ public class PlayerAnimator : MonoBehaviour
 {
     private const string IsWalking = "isWalking";
     private const string IsRunning = "isRunning";
+    private const string IsJumping = "isJumping";
 
     private Animator _animator;
     private PlayerInput _playerInput;
@@ -16,8 +17,10 @@ public class PlayerAnimator : MonoBehaviour
         _playerInput = new PlayerInput();
         _playerInput.Player.Move.performed += OnMovePerformed;
         _playerInput.Player.Run.performed += OnRunPerformed;
+        _playerInput.Player.Jump.performed += OnJumpPerformed;
         _playerInput.Player.Move.canceled += OnMoveCanceled;
         _playerInput.Player.Run.canceled += OnRunCanceled;
+        _playerInput.Player.Jump.canceled += OnJumpCancelled;
     }
 
     private void OnEnable() => _playerInput.Enable();
@@ -28,7 +31,11 @@ public class PlayerAnimator : MonoBehaviour
 
     private void OnRunPerformed(InputAction.CallbackContext _) => _animator.SetBool(IsRunning, true);
 
+    private void OnJumpPerformed(InputAction.CallbackContext _) => _animator.SetBool(IsJumping, true);
+
     private void OnMoveCanceled(InputAction.CallbackContext _) => _animator.SetBool(IsWalking, false);
 
     private void OnRunCanceled(InputAction.CallbackContext _) => _animator.SetBool(IsRunning, false);
+
+    private void OnJumpCancelled(InputAction.CallbackContext _) => _animator.SetBool(IsJumping, false);
 }
