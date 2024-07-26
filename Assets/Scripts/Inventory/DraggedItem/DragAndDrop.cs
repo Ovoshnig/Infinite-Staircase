a9929e4767406972abfe1ecaa7b3cbb59f70975e
Ovoshnig <boost45644@gmail.com>
2024-07-26 15:48:26 +0700

    Fixed items drag and drop

--- Assets/Scripts/Inventory/DraggedItem/DragAndDrop.cs
+++ Assets/Scripts/Inventory/DraggedItem/DragAndDrop.cs
@@ -1,32 +1,35 @@
+using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(RectTransform),
-                  typeof(Image))]
+[RequireComponent(typeof(Image))]
 public class DragAndDrop : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
-    private RectTransform _rectTransform;
+    [SerializeField] private Color _normalColor = new(255f, 255f, 255f, 255f);
+    [SerializeField] private Color _draggedColor = new(255f, 255f, 255f, 170f);
+
     private Image _image;
 
-    private void Awake()
-    {
-        _rectTransform = GetComponent<RectTransform>();
-        _image = GetComponent<Image>();
-    }
+    public event Action Started;
+    public event Action Ended;
+
+    private void Awake() => _image = GetComponent<Image>();
 
     public void OnBeginDrag(PointerEventData eventData)
     {
+        _image.color = _draggedColor;
         _image.raycastTarget = false;
+        Started?.Invoke();
     }
 
-    public void OnDrag(PointerEventData eventData)
-    {
-        _rectTransform.anchoredPosition += eventData.delta;
-    }
+    public void OnDrag(PointerEventData eventData) => transform.position = Mouse.current.position.ReadValue();
 
     public void OnEndDrag(PointerEventData eventData)
     {
+        _image.color = _normalColor;
         _image.raycastTarget = true;
+        Ended?.Invoke();
     }
 }
