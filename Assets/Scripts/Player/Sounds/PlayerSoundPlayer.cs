9e728e6b393d91437f8cb9215e0d60b1ede521b0
Ovoshnig <boost45644@gmail.com>
2024-08-14 12:43:32 +0700

    The sounds of the player's footsteps are synchronized with the animation

--- Assets/Scripts/Player/Sounds/PlayerSoundPlayer.cs
+++ Assets/Scripts/Player/Sounds/PlayerSoundPlayer.cs
@@ -1,83 +1,51 @@
-using Cysharp.Threading.Tasks;
 using Random = System.Random;
 using UnityEngine;
-using System.Threading;
 
-[RequireComponent(typeof(AudioSource),
-                  typeof(PlayerState))]
+[RequireComponent(typeof(AudioSource))]
 public class PlayerSoundPlayer : MonoBehaviour
 {
+    [SerializeField] private PlayerState _playerState;
+
     private AudioSource _audioSource;
-    private PlayerState _playerState;
     private AudioClip[] _footstepClips;
+    private AudioClip[] _landClips;
     private Random _random;
-    private CancellationTokenSource _cts;
 
     private void Awake()
     {
         _audioSource = GetComponent<AudioSource>();
-        _playerState = GetComponent<PlayerState>();
 
-        _playerState.WalkStarted += OnWalkStarted;
-        _playerState.WalkEnded += OnWalkEnded;
         _playerState.GroundEntered += OnGroundEntered;
 
         _random = new Random();
     }
 
-    private void Start() => _footstepClips = Resources.LoadAll<AudioClip>(ResourcesConstants.PlayerFootstepPath);
-
-    private void OnDestroy()
+    private void Start()
     {
-        _playerState.WalkStarted -= OnWalkStarted;
-        _playerState.WalkEnded -= OnWalkEnded;
-        _playerState.GroundEntered -= OnGroundEntered;
-
-        _cts?.CancelAndDispose(ref _cts);
+        _footstepClips = Resources.LoadAll<AudioClip>(ResourcesConstants.PlayerFootstepPath);
+        _landClips = Resources.LoadAll<AudioClip>(ResourcesConstants.PlayerLandPath);
     }
 
-    private void OnWalkStarted()
+    private void OnDestroy() => _playerState.GroundEntered -= OnGroundEntered;
+
+    public void PlayStepSound()
     {
-        _cts = new CancellationTokenSource();
-        PlayFootsteps().Forget();
+        AudioClip clip = GetRandomClip(_footstepClips);
+        _audioSource.PlayOneShot(clip);
     }
 
-    private void OnWalkEnded() => _cts?.CancelAndDispose(ref _cts);
+    private void OnGroundEntered() => PlayLandingSound();
 
-    private void OnGroundEntered() => PlayLanding();
-
-    private async UniTask PlayFootsteps()
+    private void PlayLandingSound()
     {
-        while (isActiveAndEnabled && !_playerState.IsInAir)
-        {
-            AudioClip clip = GetRandomClip();
-            _audioSource.clip = clip;
-            _audioSource.Play();
-
-            if (_cts.IsNullOrCanceled())
-                return;
-
-            await UniTask.WaitWhile(() => _audioSource.isPlaying, cancellationToken: _cts.Token);
-            float delay = _playerState.IsRunning ? clip.length / 5f : clip.length;
-
-            if (_cts.IsNullOrCanceled())
-                return;
-
-            await UniTask.WaitForSeconds(delay, cancellationToken: _cts.Token);
-        }
+        AudioClip clip = GetRandomClip(_landClips);
+        _audioSource.PlayOneShot(clip);
     }
 
-    private void PlayLanding()
+    private AudioClip GetRandomClip(AudioClip[] clips)
     {
-        AudioClip clip = GetRandomClip();
-        _audioSource.clip = clip;
-        _audioSource.Play();
-    }
-
-    private AudioClip GetRandomClip()
-    {
-        int index = _random.Next(0, _footstepClips.Length);
-        AudioClip clip = _footstepClips[index];
+        int index = _random.Next(0, clips.Length);
+        AudioClip clip = clips[index];
 
         return clip;
     }
