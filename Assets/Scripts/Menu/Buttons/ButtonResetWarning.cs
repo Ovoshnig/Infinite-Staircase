939cc7d49dfa390a85155c34b61b86dd05a77161
Ovoshnig <boost45644@gmail.com>
2025-03-23 13:13:51 +0700

    Made ContinueGameButton not interactable after the game reset

--- Assets/Scripts/Menu/Buttons/ButtonResetWarning.cs
+++ Assets/Scripts/Menu/Buttons/ButtonResetWarning.cs
@@ -6,18 +6,20 @@ using VContainer;
 [RequireComponent(typeof(Button))]
 public class ButtonResetWarning : MonoBehaviour
 {
+    [SerializeField] private Button _continueGameButton;
+
     private CompositeDisposable _compositeDisposable = new();
     private SaveStorage _saveStorage;
-    private Button _button;
+    private Button _resetButton;
 
     [Inject]
     public void Construct(SaveStorage saveStorage) => _saveStorage = saveStorage;
 
-    private void Awake() => _button = GetComponent<Button>();
+    private void Awake() => _resetButton = GetComponent<Button>();
 
     private void OnEnable()
     {
-        _button.OnClickAsObservable()
+        _resetButton.OnClickAsObservable()
             .Subscribe(_ => OnResetButtonClicked())
             .AddTo(_compositeDisposable);
     }
@@ -32,5 +34,7 @@ public class ButtonResetWarning : MonoBehaviour
     {
         _saveStorage.ResetData();
         _saveStorage.Set(SaveConstants.SaveCreatedKey, false);
+
+        _continueGameButton.interactable = false;
     }
 }
