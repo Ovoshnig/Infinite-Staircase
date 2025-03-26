b08511997a01e343c0616f34dfb71db3c21e95e2
Ovoshnig <boost45644@gmail.com>
2025-03-26 19:01:15 +0700

    Removed PreBuildMusicListGenerator

--- Assets/Scripts/Editor/PreBuildResourceListGenerator.cs
+++
@@ -1,9 +0,0 @@
-using UnityEditor.Build.Reporting;
-using UnityEditor.Build;
-
-public class PreBuildMusicListGenerator : IPreprocessBuildWithReport
-{
-    public int callbackOrder => 0;
-
-    public void OnPreprocessBuild(BuildReport report) => ResourcesPathsListCreator.CreateList();
-}
