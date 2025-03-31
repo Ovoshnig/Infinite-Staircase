d2d071bfd57ae41a52d07fef2bbe56e0293f9794
Ovoshnig <boost45644@gmail.com>
2025-03-31 18:17:27 +0700

    The player sounds have been redone in Addressables. They are loaded using the new PlayerSoundLoader script.

---
+++ Assets/Scripts/Player/Sounds/PlayerSoundLoader.cs
@@ -0,0 +1,36 @@
+﻿using Cysharp.Threading.Tasks;
+using System.Threading;
+using UnityEngine.AddressableAssets;
+using UnityEngine.Audio;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+public class PlayerSoundLoader
+{
+    AsyncOperationHandle<AudioResource> _footstepHandle;
+    AsyncOperationHandle<AudioResource> _landHandle;
+
+    public async UniTask<(AudioResource footstepResource, AudioResource landResource)> LoadSoundsAsync(
+        AssetReference footstepReference, AssetReference landReference, CancellationToken token)
+    {
+        AudioResource footstepResource = null;
+        AudioResource landResource = null;
+
+        _footstepHandle = Addressables.LoadAssetAsync<AudioResource>(footstepReference);
+        await _footstepHandle.ToUniTask(cancellationToken: token);
+        if (_footstepHandle.Status == AsyncOperationStatus.Succeeded)
+            footstepResource = _footstepHandle.Result;
+
+        _landHandle = Addressables.LoadAssetAsync<AudioResource>(landReference);
+        await _landHandle.ToUniTask(cancellationToken: token);
+        if (_landHandle.Status == AsyncOperationStatus.Succeeded)
+            landResource = _landHandle.Result;
+
+        return (footstepResource, landResource);
+    }
+
+    public void ReleaseSounds()
+    {
+        _footstepHandle.Release();
+        _landHandle.Release();
+    }
+}
--- Assets/Scripts/Player/Sounds/PlayerSoundPlayer.cs
+++ Assets/Scripts/Player/Sounds/PlayerSoundPlayer.cs
@@ -1,29 +1,53 @@
+using Cysharp.Threading.Tasks;
 using JetBrains.Annotations;
 using R3;
+using System.Threading;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 using UnityEngine.Audio;
 using VContainer;
 
 [RequireComponent(typeof(AudioSource))]
 public class PlayerSoundPlayer : MonoBehaviour
 {
-    [SerializeField] private AudioResource _footstepResource;
-    [SerializeField] private AudioResource _landResource;
+    [SerializeField] private AssetReference _footstepReference;
+    [SerializeField] private AssetReference _landReference;
 
-	private PlayerState _playerState;
+    private PlayerState _playerState;
 	private AudioSource _audioSource;
+    private PlayerSoundLoader _soundLoader;
+    private AudioResource _footstepResource;
+    private AudioResource _landResource;
 
     [Inject]
     public void Construct(PlayerState playerState) => _playerState = playerState;
 
-    private void Awake() => _audioSource = GetComponent<AudioSource>();
-
-    private void Start()
+    private void Awake() 
     {
+        _audioSource = GetComponent<AudioSource>();
+
+        _soundLoader = new PlayerSoundLoader();
+    }
+
+    private async void Start()
+    {
+        using CancellationTokenSource cts = new();
+        var (footstepResource, landResource) = await _soundLoader
+            .LoadSoundsAsync(_footstepReference, _landReference, cts.Token);
+        _footstepResource = footstepResource;
+        _landResource = landResource;
+
         _playerState.IsGrounded
             .Where(value => value)
             .Subscribe(_ => PlayLandSound())
             .AddTo(this);
+    }
+
+    private void OnDestroy()
+    {
+        _soundLoader.ReleaseSounds();
+        Resources.UnloadAsset(_footstepResource);
+        Resources.UnloadAsset(_landResource);
     }
 
     [UsedImplicitly]
