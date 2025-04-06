46af33c1e4a3f4acf7fa2548b6338cbad6d8a355
Ovoshnig <boost45644@gmail.com>
2025-04-06 22:56:35 +0700

    The scripts with constants have been moved to the appropriate folders

---
+++ Assets/Scripts/Audio/Music/Resources/ResourcesConstants.cs
@@ -0,0 +1,6 @@
+public static class ResourcesConstants
+{
+    public const string ResourcesPath = "Assets/Resources";
+    public const string ResourcesListName = "resourcesList";
+    public const string MusicPath = "Audio/Music";
+}
--- Assets/Scripts/Constants/PlayerConstants.cs
+++
@@ -1,5 +0,0 @@
-﻿public static class PlayerConstants
-{
-    public const string PlayerEyeCenterName = "EyeCenter";
-    public const string PlayerHeadCenterName = "HeadCenter";
-}
