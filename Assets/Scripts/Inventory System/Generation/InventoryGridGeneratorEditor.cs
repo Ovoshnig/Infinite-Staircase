b0e9b39d9136f8eb8e6c4110f371fcc33e58f45b
Ovoshnig <boost45644@gmail.com>
2025-06-30 01:25:36 +0700

    Small refactoring in InventoryGridGeneratorEditor

--- Assets/Scripts/Inventory System/Generation/InventoryGridGeneratorEditor.cs
+++ Assets/Scripts/Inventory System/Generation/InventoryGridGeneratorEditor.cs
@@ -1,5 +1,4 @@
 using UnityEditor;
-using UnityEditor.SceneManagement;
 using UnityEngine;
 
 [CustomEditor(typeof(InventoryGridGenerator))]
@@ -12,11 +11,6 @@ public class InventoryGridGeneratorEditor : Editor
         InventoryGridGenerator generator = (InventoryGridGenerator)target;
 
         if (GUILayout.Button("Generate Inventory Grid"))
-        {
-            Undo.RegisterFullObjectHierarchyUndo(generator.gameObject, "Generate Inventory Grid");
             generator.TryGenerate();
-            EditorUtility.SetDirty(generator.gameObject);
-            EditorSceneManager.MarkSceneDirty(generator.gameObject.scene);
-        }
     }
 }
