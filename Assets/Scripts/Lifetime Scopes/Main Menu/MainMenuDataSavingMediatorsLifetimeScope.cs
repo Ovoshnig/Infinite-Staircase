c3a8da9d9ce40c207e07b1ae8784dfca5535a12b
Ovoshnig <boost45644@gmail.com>
2025-07-10 22:10:49 +0700

    Returned the erased code to MainMenuDataSavingMediatorsLifetimeScope

--- Assets/Scripts/Lifetime Scopes/Main Menu/MainMenuDataSavingMediatorsLifetimeScope.cs
+++ Assets/Scripts/Lifetime Scopes/Main Menu/MainMenuDataSavingMediatorsLifetimeScope.cs
@@ -5,5 +5,23 @@ public class MainMenuDataSavingMediatorsLifetimeScope : LifetimeScope
 {
     protected override void Configure(IContainerBuilder builder)
     {
+        MainMenuCanvasView canvasView = FindFirstObjectByType<MainMenuCanvasView>();
+        WarningResetButtonView warningResetButtonView = canvasView
+            .GetComponentInChildren<WarningResetButtonView>(true);
+
+        if (warningResetButtonView != null)
+        {
+            builder.RegisterInstance(warningResetButtonView);
+            builder.RegisterEntryPoint<SaveStorageWarningResetButtonViewMediator>(Lifetime.Singleton);
+        }
+
+        AchievedLevelButtonView achievedLevelButtonView = canvasView
+            .GetComponentInChildren<AchievedLevelButtonView>(true);
+
+        if (achievedLevelButtonView != null)
+        {
+            builder.RegisterInstance(achievedLevelButtonView);
+            builder.RegisterEntryPoint<SaveStorageAchievedLevelButtonViewMediator>(Lifetime.Singleton);
+        }
     }
 }
