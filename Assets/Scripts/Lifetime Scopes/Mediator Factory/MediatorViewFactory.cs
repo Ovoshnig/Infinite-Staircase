e5acf47d830c0351c1a142062f2069f9a043c451
Ovoshnig <boost45644@gmail.com>
2025-07-11 22:16:39 +0700

    Added abstract override TMediator Create in MediatorViewFactory

--- Assets/Scripts/Lifetime Scopes/Mediator Factory/MediatorViewFactory.cs
+++ Assets/Scripts/Lifetime Scopes/Mediator Factory/MediatorViewFactory.cs
@@ -4,10 +4,12 @@ public abstract class MediatorViewFactory<TMediator, TView> : MediatorFactory<TM
     where TMediator : Mediator
     where TView : Component
 {
+    public abstract override TMediator Create(TView view);
 }
 
 public abstract class MediatorViewFactory<TMediator, TDependency, TView> : MediatorFactory<TMediator, TDependency, TView>
     where TMediator : Mediator
     where TView : Component
 {
+    public abstract override TMediator Create(TDependency dependency, TView view);
 }
