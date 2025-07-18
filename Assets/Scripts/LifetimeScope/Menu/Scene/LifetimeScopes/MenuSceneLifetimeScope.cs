4ffedf4652bfd0757267577d0ec71c4c59dd7076
Ovoshnig <boost45644@gmail.com>
2025-07-18 16:04:58 +0700

    Renamed and reorganized mediators, factories and LifetimeScops between Menu and Scene

--- Assets/Scripts/LifetimeScope/Menu/Scene/LifetimeScopes/MenuSceneLifetimeScope.cs
+++ Assets/Scripts/LifetimeScope/Menu/Scene/LifetimeScopes/MenuSceneLifetimeScope.cs
@@ -1,9 +1,9 @@
 using VContainer;
 using VContainer.Unity;
 
-public class MenuSceneMediatorsLifetimeScope : LifetimeScope
+public class MenuSceneLifetimeScope : LifetimeScope
 {
-    protected override void Configure(IContainerBuilder builder) => 
+    protected override void Configure(IContainerBuilder builder) =>
         builder.Register<SceneSwitchButtonViewMediatorFactory>(Lifetime.Singleton);
 
     private void Start()
